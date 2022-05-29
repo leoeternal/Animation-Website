c36fd3c614fcd22fe68a2fa87afea83630d23e78
Nikhil Jindal <nikhiljindal@Nikhils-MacBook-Air.local>
2022-05-30 01:26:30 +0530

    header responsive

--- src/components/static/Header.js
+++ src/components/static/Header.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
-// import Button from "@mui/material/Button";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import logo from "../../static/images/logo.jpeg";
+import DensityMediumIcon from "@mui/icons-material/DensityMedium";
 import "./header.css";
 
 function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const [navbar, setNavbar] = useState(false);
 
   window.addEventListener("scroll", () => {
@@ -24,7 +35,7 @@ function Header() {
           <p>MarkitUp</p>
         </div>
       </div>
-      {/* <div className="links">
+      <div className="links">
         <div className="link">
           <a href="#first">
             <p>Home</p>
@@ -32,7 +43,7 @@ function Header() {
         </div>
         <div className="link">
           <a href="#second">
-            <p>Our Services</p>
+            <p style={{ textAlign: "center" }}>Our Services</p>
           </a>
         </div>
         <div className="link">
@@ -46,13 +57,51 @@ function Header() {
             <p>Work</p>
           </a>
         </div>
-
-        <div className="link">
-          <Button size="small" variant="contained">
-            Contact Us
-          </Button>
-        </div>
-      </div> */}
+      </div>
+      <div className="button">
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <DensityMediumIcon style={{ color: "black" }} />
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}>
+            <a style={{ textDecoration: "none", color: "black" }} href="#first">
+              Home
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a
+              style={{ textDecoration: "none", color: "black" }}
+              href="#second"
+            >
+              Our Services
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a style={{ textDecoration: "none", color: "black" }} href="#four">
+              About
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a style={{ textDecoration: "none", color: "black" }} href="#five">
+              Work
+            </a>
+          </MenuItem>
+        </Menu>
+      </div>
     </div>
   );
 }
